cabb8634a9f7cd23583fb3406cd6abec22578ace
Pahuljot Matharoo <matharoopahuljot@gmail.com>
2025-06-19 17:00:20 -0400

    sql table creation query added

---
+++ TableCreation.sql
@@ -0,0 +1,19 @@
+-- =========================================
+-- Create FileTable template
+-- =========================================
+
+ CREATE TABLE UFC_HISTORY(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	AccountID INT,
+	Fighter1 NVARCHAR(256),
+	Fighter2 NVARCHAR(256),
+	Percentage1 float,
+	Percentage2 float,
+	Winner NVARCHAR(256),
+	)
+
+CREATE TABLE login_info(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	username NVARCHAR(256),
+	password NVARCHAR(256),
+	)
